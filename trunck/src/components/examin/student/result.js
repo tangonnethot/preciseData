58166e8ea0cbee4040c746d0d73951d8a929971a
freeflyAn <anting158@163.com>
2020-03-26 15:43:59 +0800

    题目结果页面支持批改（屏蔽订正答案）

--- trunck/src/components/examin/student/result.js
+++ trunck/src/components/examin/student/result.js
@@ -36,15 +36,19 @@ class Result extends React.PureComponent{
               { 
                 question.topics && question.topics.length>0 && question.topics.map( (child,index) => {
                   let arrAnswer = this.props.userAnswer.split(";");
-                  let arrRAnswer = this.props.revisedAnswer.split(";");
                   let arrScore = this.props.userScore.split(";");
-                  const childProps = Object.assign({},this.props,{
+                  let childProps = Object.assign({},this.props,{
                     question:child,
                     questionIndex:child.topicNo,
                     userAnswer:arrAnswer[index],
-                    rAnswer:arrRAnswer[index],
                     userScore:arrScore[index]
-                  })
+                  });
+                  if( this.props.revisedAnswer ){
+                    let arrRAnswer = this.props.revisedAnswer.split(";");
+                    childProps={...childProps,
+                      rAnswer:arrRAnswer[index],
+                    }
+                  }
                   return <Fragment key={child.id}>
                     {
                       child.qtype == 1076 && <ResultHeaderChoice {...childProps} />
