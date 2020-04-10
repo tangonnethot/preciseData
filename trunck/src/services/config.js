f0f60877a7b1107896720dfc53b6421c0597f503
tangyaojun <tangyaojun@mainbo.com>
2020-04-10 10:22:51 +0800

    去掉不需要的配置

--- trunck/src/services/config.js
+++ trunck/src/services/config.js
@@ -1,10 +1,9 @@
-const prefix = '/';
+const prefix = '';
 let coursePrefix,questionBankPrefix , taskPrefix , studySituationPrefix , ErrorBookPrefix,htmlPrefix;
 
 //正式环境使用，测试环境屏蔽该行代码 
 //start
  coursePrefix = questionBankPrefix = taskPrefix = studySituationPrefix = ErrorBookPrefix = 'precise-web';
- htmlPrefix = '/web'
 //end
 
 //测试环境使用，正式环境打包时屏蔽下面代码 
@@ -19,7 +18,6 @@ let coursePrefix,questionBankPrefix , taskPrefix , studySituationPrefix , ErrorB
 
 export {
   prefix,
-  htmlPrefix,
   coursePrefix,
   questionBankPrefix,
   taskPrefix,
